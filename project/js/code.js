cb4d77a9a458afa98a3a62a23ebab92b11b6feb9
DrTlKl <cs.zsombi03@gmail.com>
2021-05-27 19:11:48 +0200

    NAV működésbe hozása, icon, kevés tartalom

--- project/js/code.js
+++ project/js/code.js
@@ -5,5 +5,39 @@ function $(id){
 }
 
 function init(){
-
+    let fooldalNav = $("fooldalNav");
+    let menuNav = $("menuNav");
+    let szallitasNav= $("szallitasNav");
+    fooldalNav.addEventListener("click", fooldalValto, false);
+    menuNav.addEventListener("click", menuValto, false);
+    szallitasNav.addEventListener("click", szallitasValto, false);
 }
+
+function fooldalValto(){
+    navReset();
+    $("fooldal").style.visibility="visible";
+    $("fooldalNavLi").className="nav-item active";
+}
+
+function menuValto(){
+    navReset();
+    $("menu").style.visibility="visible";
+    $("menuNavLi").className="nav-item active";
+}
+
+function szallitasValto(){
+    navReset();
+    $("szallitas").style.visibility="visible";
+    $("szallitasNavLi").className="nav-item active";
+}
+
+function navReset(){
+    $("fooldal").style.visibility="hidden";
+    $("menu").style.visibility="hidden";
+    $("szallitas").style.visibility="hidden";
+    $("fooldalNavLi").className="nav-item";
+    $("menuNavLi").className="nav-item";
+    $("szallitasNavLi").className="nav-item";
+}
+
+
