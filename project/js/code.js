c44fea0a29cc10b6b1df8e2278babc49296ed349
DrTlKl <cs.zsombi03@gmail.com>
2021-05-29 20:55:50 +0200

    Kosár rendszer- back end
    Hozzáadtam az ételrendelés alapjára szolgáló metódusokat, így már a kosár készen van, csak meg kell jeleníteni a tartalmát

--- project/js/code.js
+++ project/js/code.js
@@ -8,7 +8,74 @@ function init(){
     $("fooldalNav").addEventListener("click", fooldalValto, false);
     $("menuNav").addEventListener("click", menuValto, false);
     $("szallitasNav").addEventListener("click", szallitasValto, false);
-    $("kosarNav").addEventListener("click",kosarValto,false)
+    $("kosarNav").addEventListener("click",kosarValto,false);
+    $("card1price").addEventListener("click", kosarba, false);
+    $("card2price").addEventListener("click", kosarba, false);
+    $("card3price").addEventListener("click", kosarba, false);
+    $("card4price").addEventListener("click", kosarba, false);
+    $("card5price").addEventListener("click", kosarba, false);
+    $("card6price").addEventListener("click", kosarba, false);
+    $("card7price").addEventListener("click", kosarba, false);
+    $("card8price").addEventListener("click", kosarba, false);
+    $("card9price").addEventListener("click", kosarba, false);
+    $("card10price").addEventListener("click", kosarba, false);
+    $("card11price").addEventListener("click", kosarba, false);
+    $("card12price").addEventListener("click", kosarba, false);
+    $("card13price").addEventListener("click", kosarba, false);
+    $("card14price").addEventListener("click", kosarba, false);
+    $("card15price").addEventListener("click", kosarba, false);
+    $("card16price").addEventListener("click", kosarba, false);
+}
+
+let kosar=[];
+
+function kosarba(e){
+    let card = e.target.id+"";
+    card = card.substring(0, 6)
+    if(card.charAt(card.length-1)=='p'){
+        card=card.substring(0,5);
+    }
+
+    let etel = peldanyosito(card)
+    let vanBenne= kosarScanner(etel.nev);
+
+    if( vanBenne > -1){
+        kosar[vanBenne].mennyiseg++;
+    }else{
+        kosar.push(etel);
+    }
+
+    let osszesen=0;
+
+    for(let i = 0; i < kosar.length; i++){
+        osszesen +=kosar[i].mennyiseg;
+    }
+
+    $("badge").innerText=osszesen;
+
+    console.log(kosar);
+}
+
+function kosarScanner(name){
+    let index = -1;
+
+    for(let i = 0; i < kosar.length; i++){
+        if(kosar[i].nev==name){
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+function peldanyosito(cardId){
+    let etel = {
+        nev: $(cardId+"name").innerText,
+        ar: $(cardId+"price").value.split(" ")[0],
+        mennyiseg: 1
+    }
+
+    return etel;
 }
 
 function fooldalValto(){
