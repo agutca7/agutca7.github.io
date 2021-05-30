de1e722cbfe948d4653ff273b5cb3ff727615a60
Gerzsenyi Patrik <patrikchannelmc@gmail.com>
2021-05-30 15:05:49 +0200

    Kártyák, html váltás, kisebb grafika igazítások

--- project/js/code.js
+++ project/js/code.js
@@ -5,10 +5,6 @@ function $(id){
 }
 
 function init(){
-    $("fooldalNav").addEventListener("click", fooldalValto, false);
-    $("menuNav").addEventListener("click", menuValto, false);
-    $("szallitasNav").addEventListener("click", szallitasValto, false);
-    $("kosarNav").addEventListener("click",kosarValto,false);
     $("card1price").addEventListener("click", kosarba, false);
     $("card2price").addEventListener("click", kosarba, false);
     $("card3price").addEventListener("click", kosarba, false);
@@ -76,39 +72,4 @@ function peldanyosito(cardId){
     }
 
     return etel;
-}
-
-function fooldalValto(){
-    navReset();
-    $("fooldal").style.visibility="visible";
-    $("fooldalNavLi").className="nav-item active";
-}
-
-function menuValto(){
-    navReset();
-    $("menu").style.visibility="visible";
-    $("menuNavLi").className="nav-item active";
-}
-
-function szallitasValto(){
-    navReset();
-    $("szallitas").style.visibility="visible";
-    $("szallitasNavLi").className="nav-item active";
-}
-
-function kosarValto(){
-    navReset();
-    $("kosar").style.visibility="visible";
-    $("kosarNavLi").className="nav-item active";
-}
-
-function navReset(){
-    $("fooldal").style.visibility="hidden";
-    $("menu").style.visibility="hidden";
-    $("szallitas").style.visibility="hidden";
-    $("kosar").style.visibility="hidden";
-    $("fooldalNavLi").className="nav-item";
-    $("menuNavLi").className="nav-item";
-    $("szallitasNavLi").className="nav-item";
-    $("kosarNavLi").className="nav-item";
 }
